f8db4b68b3048f47e4cc077eba6c17b34c23de5e
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-22 16:42:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -208,7 +208,7 @@ def big_shoe_rebounds
   rebounds = 0
   game_hash.each do |team, team_info|
       team_info[:players].each do |player|
-        if player 
+        if player[:shoe]
 
       end
     end
